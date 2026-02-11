d903d904e38ca9cd0b8194c3215274cfe7c0d17e
“maximoskp” <maximoskalpap@gmail.com>
2026-02-11 07:53:04 +0200

    weighted

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.nn import TransformerEncoderLayer
 from torch_geometric.data import Data, Batch
+from collections import defaultdict
 import math
 from copy import deepcopy
 
@@ -1389,22 +1390,122 @@ class SimpleDE(nn.Module):
 # -----------------------------
 # Helper: turn chord sequence to PyG graph
 # -----------------------------
-def make_graph_from_chords(chord_ids_sequence, chord_id_features):
+
+def compute_edge_features(src_feat, dst_feat):
+    """
+    src_feat, dst_feat: torch tensors of shape [24]
+    
+    Returns:
+        torch.tensor([normalized_fifths_distance, pitch_class_overlap])
+    """
+    
+    # ---- Root extraction ----
+    src_root = torch.argmax(src_feat[:12]).item()
+    dst_root = torch.argmax(dst_feat[:12]).item()
+    
+    # Map to circle of fifths index
+    src_circle = (src_root * 7) % 12
+    dst_circle = (dst_root * 7) % 12
+    
+    # Circular distance
+    diff = abs(src_circle - dst_circle)
+    fifths_distance = min(diff, 12 - diff)
+    
+    # Normalize to [0,1]
+    normalized_fifths_distance = fifths_distance / 6.0
+    
+    # ---- Pitch-class overlap (Jaccard similarity) ----
+    src_pcs = src_feat[12:].bool()
+    dst_pcs = dst_feat[12:].bool()
+    
+    intersection = torch.logical_and(src_pcs, dst_pcs).sum().item()
+    union = torch.logical_or(src_pcs, dst_pcs).sum().item()
+    
+    if union == 0:
+        pitch_overlap = 0.0
+    else:
+        pitch_overlap = intersection / union
+    
+    return torch.tensor([normalized_fifths_distance, pitch_overlap], dtype=torch.float)
+# end compute_edge_features
+
+# def make_graph_from_chords(chord_ids_sequence, chord_id_features):
+#     unique_ids = list(set(chord_ids_sequence))
+#     id_to_idx = {cid: i for i, cid in enumerate(unique_ids)}
+
+#     x = torch.stack([torch.LongTensor(chord_id_features[cid]) for cid in unique_ids])  # [num_unique_nodes, 24]
+
+#     edges = []
+#     for i in range(len(chord_ids_sequence) - 1):
+#         src = id_to_idx[chord_ids_sequence[i]]
+#         dst = id_to_idx[chord_ids_sequence[i + 1]]
+#         edges.append((src, dst))
+
+#     edge_index = torch.tensor(edges, dtype=torch.long).T  # [2, num_edges]
+    
+#     return Data(x=x, edge_index=edge_index)
+# # end make_graph_from_chords
+
+def make_weighted_graph_from_chords(chord_ids_sequence, chord_id_features, use_probabilities=False):
+    
     unique_ids = list(set(chord_ids_sequence))
     id_to_idx = {cid: i for i, cid in enumerate(unique_ids)}
 
-    x = torch.stack([torch.LongTensor(chord_id_features[cid]) for cid in unique_ids])  # [num_unique_nodes, 24]
+    # ---- Node feature matrix ----
+    x = torch.stack([
+        torch.tensor(chord_id_features[cid], dtype=torch.float)
+        for cid in unique_ids
+    ])
 
-    edges = []
+    # ---- Count transitions ----
+    transition_counts = defaultdict(int)
+    outgoing_counts = defaultdict(int)
+
     for i in range(len(chord_ids_sequence) - 1):
-        src = id_to_idx[chord_ids_sequence[i]]
-        dst = id_to_idx[chord_ids_sequence[i + 1]]
-        edges.append((src, dst))
+        src_id = chord_ids_sequence[i]
+        dst_id = chord_ids_sequence[i + 1]
 
-    edge_index = torch.tensor(edges, dtype=torch.long).T  # [2, num_edges]
-    
-    return Data(x=x, edge_index=edge_index)
-# end make_graph_from_chords
+        if src_id in id_to_idx and dst_id in id_to_idx:
+            transition_counts[(src_id, dst_id)] += 1
+            outgoing_counts[src_id] += 1
+
+    # ---- Build edges ----
+    edge_index = []
+    edge_weight = []
+    edge_attr = []
+
+    for (src_id, dst_id), count in transition_counts.items():
+        
+        src_idx = id_to_idx[src_id]
+        dst_idx = id_to_idx[dst_id]
+
+        edge_index.append([src_idx, dst_idx])
+
+        # weight = count or probability
+        if use_probabilities:
+            weight = count / outgoing_counts[src_id]
+        else:
+            weight = float(count)
+
+        edge_weight.append(weight)
+
+        # compute edge features
+        src_feat = x[src_idx]
+        dst_feat = x[dst_idx]
+        ef = compute_edge_features(src_feat, dst_feat)
+        edge_attr.append(ef)
+
+    edge_index = torch.tensor(edge_index, dtype=torch.long).T  # [2, num_edges]
+    edge_weight = torch.tensor(edge_weight, dtype=torch.float)
+    edge_attr = torch.stack(edge_attr)  # [num_edges, 2]
+
+    return Data(
+        x=x,
+        edge_index=edge_index,
+        edge_weight=edge_weight,
+        edge_attr=edge_attr
+    )
+# end make_weighted_graph_from_chords
 
 def remove_consecutive_duplicates(lst):
     if not lst:
@@ -1429,7 +1530,8 @@ def remove_out_of_dict_ids(lst, d):
 def make_graph_from_input_ids(chord_id_duplicates_sequence, chord_id_features):
     chord_ids_sequence = remove_consecutive_duplicates( chord_id_duplicates_sequence )
     chord_ids_sequence = remove_out_of_dict_ids( chord_ids_sequence, chord_id_features )
-    return make_graph_from_chords(chord_ids_sequence, chord_id_features)
+    return make_weighted_graph_from_chords(chord_ids_sequence, chord_id_features, use_probabilities=True)
+    # return make_graph_from_chords(chord_ids_sequence, chord_id_features)
 # end make_graph_from_input_ids
 
 # -----------------------------
